f368e31c6957f934943f8fd60b7e7c336ed639f0
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-30 07:12:46 +0800

    增加上传图片功能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # Needed for Devise
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Needed for Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
