53b6338ebcf4d3bc9c33657f15783c4d2282f569
zhangwei-dayue <zhangwei19890518@gmail.com>
2017-07-30 09:53:38 +0800

    增加评论评星的功能

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,12 @@ class MoviesController < ApplicationController
 
   def show
     @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
+    if @reviews.blank?
+      @avg_rating = 0
+    else
+      @avg_rating = @reviews.average(:rating).round(2)
+    end
   end
 
   def new
